0f92cd32ecb53b2d6f5b5ec68498b12d6137a085
Kusters,Damien D.G.P <damien.kusters@student.fontys.nl>
2021-10-05 12:51:30 +0200

    Add basic models

---
+++ lpr.Common/Models/Version.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lpr.Common.Models
+{
+    public class Version
+    {
+        public string SemanticVersion {  get; set; }
+    }
+}
