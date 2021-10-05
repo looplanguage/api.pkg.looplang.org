cf463c493ecf9ec6c3976ff597a48dc9b66b3f7f
Kusters,Damien D.G.P <damien.kusters@student.fontys.nl>
2021-10-05 11:20:47 +0200

    Add sample controller

---
+++ lpr.WebAPI/Controllers/SampleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lpr.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SampleController : ControllerBase
+    {
+        /// <summary>
+        ///     Retrieves a user by their ID.
+        /// </summary>
+        /// <param name="id">The ID of the searched user.</param>  
+        /// <response code="200">Returns the found user.</response>
+        /// <response code="500">A Server error has occured.</response>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> getUserById(int id)
+        {
+            return StatusCode(200, id);
+        }
+    }
+}
