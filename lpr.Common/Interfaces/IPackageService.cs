31846e1d7b19f940aecb7a76da4cf90d4730a43a
nickonos <70724339+nickonos@users.noreply.github.com>
2021-10-12 14:03:05 +0200

    Fixed merge errors

--- lpr.Common/Interfaces/IPackageService.cs
+++ lpr.Common/Interfaces/IPackageService.cs
@@ -1,7 +1,6 @@
 ﻿using lpr.Common.Models;
 using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
