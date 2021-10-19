78101b1887b7b275469c6132fa8064c79a44fe56
Restyled.io <commits@restyled.io>
2021-10-19 20:59:09 +0000

    Restyled by astyle

--- lpr.Common/Interfaces/Contexts/ILprDbContext.cs
+++ lpr.Common/Interfaces/Contexts/ILprDbContext.cs
@@ -8,10 +8,22 @@ using System.Threading.Tasks;
 using Version = lpr.Common.Models.Version;
 
 namespace lpr.Common.Interfaces.Contexts {
-  public interface ILprDbContext : IDbContext, IDisposable {
-    public DbSet<Account> Account { get; set; }
-    public DbSet<Organisation> Organisation { get; set; }
-    public DbSet<Package> Package { get; set; }
-    public DbSet<Version> Version { get; set; }
-  }
+public interface ILprDbContext : IDbContext, IDisposable {
+    public DbSet<Account> Account {
+        get;
+        set;
+    }
+    public DbSet<Organisation> Organisation {
+        get;
+        set;
+    }
+    public DbSet<Package> Package {
+        get;
+        set;
+    }
+    public DbSet<Version> Version {
+        get;
+        set;
+    }
+}
 }
--- lpr.WebAPI/Program.cs
+++ lpr.WebAPI/Program.cs
@@ -16,31 +16,33 @@ builder.Services.AddScoped<IPackageService, PackageService>();
 builder.Services.AddScoped<IPackageData, PackageData>();
 
 builder.Services.AddDbContext<ILprDbContext, LprContext>(options => {
-  options.UseMySql(
-      "server=localhost; user id =root; password=root; database=LPR; ",
-      new MariaDbServerVersion(new Version(10, 5, 9)));
+    options.UseMySql(
+        "server=localhost; user id =root; password=root; database=LPR; ",
+        new MariaDbServerVersion(new Version(10, 5, 9)));
 });
 
 using (var scope = builder.Services.BuildServiceProvider().CreateScope()) {
-  using (var context = scope.ServiceProvider.GetService<ILprDbContext>()) {
-    context.Database.EnsureCreated();
-  }
+    using (var context = scope.ServiceProvider.GetService<ILprDbContext>()) {
+        context.Database.EnsureCreated();
+    }
 }
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c => {
-  c.SwaggerDoc("v1", new() { Title = "lpr.WebAPI", Version = "v1" });
+    c.SwaggerDoc("v1", new() {
+        Title = "lpr.WebAPI", Version = "v1"
+    });
 });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
-  app.UseSwagger();
-  app.UseSwaggerUI(c => {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "lpr.WebAPI v1");
-    c.RoutePrefix = string.Empty;
-  });
+    app.UseSwagger();
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "lpr.WebAPI v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
