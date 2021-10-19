4a35f9160925d8d8a3fb1da3762e5976512c7f34
Restyled.io <commits@restyled.io>
2021-10-19 21:08:20 +0000

    Restyled by clang-format

--- lpr.WebAPI/Controllers/SampleController.cs
+++ lpr.WebAPI/Controllers/SampleController.cs
@@ -4,17 +4,12 @@ using lpr.Common.Interfaces.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace lpr.WebAPI.Controllers
-{
-[ApiController]
-[Route("[controller]")]
-public class SampleController : ControllerBase
-{
+namespace lpr.WebAPI.Controllers {
+  [ApiController]
+  [Route("[controller]")]
+  public class SampleController : ControllerBase {
     private readonly ISampleService _srv;
-    public SampleController(ISampleService srv)
-    {
-        _srv = srv;
-    }
+    public SampleController(ISampleService srv) { _srv = srv; }
 
     /// <summary>
     ///     Retrieves a user by their ID.
@@ -25,11 +20,10 @@ public class SampleController : ControllerBase
     [HttpGet("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> GetUserById(int id)
-    {
-        int output = await _srv.getUserById(id);
+    public async Task<IActionResult> GetUserById(int id) {
+      int output = await _srv.getUserById(id);
 
-        return StatusCode(200, output);
+      return StatusCode(200, output);
     }
-}
+  }
 }
