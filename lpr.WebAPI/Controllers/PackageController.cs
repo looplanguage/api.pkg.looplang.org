8883aa9d988c9622de97527d611a2439073dc016
Neal Geilen <44571222+NealGeilen@users.noreply.github.com>
2021-10-20 11:30:25 +0200

    Feat/13 docker (#27)
    * feat: docker testing for build
    
    * feat: Docker added maria DB
    
    * feat: trying to make Docker work
    
    * feat: Docker works... Now the api it self
    
    * Make project runnable with docker-compose
    
    * feat: api and database and docker working
    
    * Restyled by astyle
    
    * Restyled by clang-format
    
    * Restyled by prettier-yaml
    
    * Restyled by whitespace
    
    * fix: build test added prerelease
    
    Co-authored-by: Kusters,Damien D.G.P <damien.kusters@student.fontys.nl>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- lpr.WebAPI/Controllers/PackageController.cs
+++ lpr.WebAPI/Controllers/PackageController.cs
@@ -1,28 +1,24 @@
-﻿using lpr.Common.Interfaces;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using lpr.Common.Interfaces;
 using lpr.Common.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace lpr.WebAPI.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class PackageController : ControllerBase
-    {
-        private readonly IPackageService _srv;
-        public PackageController(IPackageService srv)
-        {
-            _srv = srv;
-        }
+namespace lpr.WebAPI.Controllers {
+  [ApiController]
+  [Route("[controller]")]
+  public class PackageController : ControllerBase {
+    private readonly IPackageService _srv;
+    public PackageController(IPackageService srv) { _srv = srv; }
 
-        // GET: PackageController
-        [HttpGet()]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> Index(int page, int amount)
-        {
-            List<Package> output = await _srv.GetPackagesPaginatedAsync(page, amount);
-            return StatusCode(200, output);
-        }
+    // GET: PackageController
+    [HttpGet()]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> Index(int page, int amount) {
+      List<Package> output = await _srv.GetPackagesPaginatedAsync(page, amount);
+      return StatusCode(200, output);
     }
+  }
 }
