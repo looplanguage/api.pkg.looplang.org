e64608130d92897be0f47c6993b5da692f1f1bc3
Restyled.io <commits@restyled.io>
2021-11-02 11:10:36 +0000

    Restyled by astyle

--- lpr.Common/Interfaces/Data/IPackageData.cs
+++ lpr.Common/Interfaces/Data/IPackageData.cs
@@ -1,7 +1,7 @@
 ﻿using lpr.Common.Models;
 
 namespace lpr.Common.Interfaces.Data {
-  public interface IPackageData {
+public interface IPackageData {
     public Task<List<Package>> GetPackagesPaginatedAsync(int page, int amount);
     public Task<List<Package>> GetTopPackagesAsync(int amount);
 
@@ -10,5 +10,5 @@ namespace lpr.Common.Interfaces.Data {
     CreatePackageAsync(Package newPackage);                   // TODO: Add auth
     public Task<Package> GetFullPackageAsync(Guid packageId); // TODO: Add auth
     public Task<Package> ArchivePackageAsync(Guid packageId); // TODO: Add auth
-  }
+}
 }
--- lpr.Common/Interfaces/Services/IPackageService.cs
+++ lpr.Common/Interfaces/Services/IPackageService.cs
@@ -1,7 +1,7 @@
 ﻿using lpr.Common.Models;
 
 namespace lpr.Common.Interfaces.Services {
-  public interface IPackageService {
+public interface IPackageService {
     public Task<List<Package>> GetPackagesPaginatedAsync(int page, int amount);
     public Task<List<Package>> GetTopPackagesAsync(int amount);
     public Task<List<Package>> GetPackagesFromOrganisationAsync(Guid organisationId);
@@ -9,5 +9,5 @@ namespace lpr.Common.Interfaces.Services {
     CreatePackageAsync(Package newPackage);                   // TODO: Add auth
     public Task<Package> GetFullPackageAsync(Guid packageId); // TODO: Add auth
     public Task<Package> ArchivePackageAsync(Guid packageId); // TODO: Add auth
-  }
+}
 }
