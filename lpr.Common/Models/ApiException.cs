38efd2cdd6c3af21e41887398dc08aef299d2f4e
Restyled.io <commits@restyled.io>
2021-11-02 14:54:13 +0000

    Restyled by clang-format

--- lpr.Common/Models/ApiException.cs
+++ lpr.Common/Models/ApiException.cs
@@ -4,22 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lpr.Common.Models
-{
-public class ApiException : Exception
-{
-    public int ErrorCode {
-        get;
-        set;
+namespace lpr.Common.Models {
+  public class ApiException : Exception {
+    public int ErrorCode { get; set; }
+    public ErrorMessage ErrorMessage { get; set; }
+    public ApiException(int errorCode, ErrorMessage errorMessage) {
+      ErrorCode = errorCode;
+      ErrorMessage = errorMessage;
     }
-    public ErrorMessage ErrorMessage {
-        get;
-        set;
-    }
-    public ApiException(int errorCode, ErrorMessage errorMessage)
-    {
-        ErrorCode = errorCode;
-        ErrorMessage = errorMessage;
-    }
-}
+  }
 }
