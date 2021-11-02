6d9c451703c8679f69c8bb153574a0fc7f716b38
Restyled.io <commits@restyled.io>
2021-11-02 10:37:07 +0000

    Restyled by clang-format

--- lpr.Logic/Services/PackageService.cs
+++ lpr.Logic/Services/PackageService.cs
@@ -10,35 +10,34 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace lpr.Logic.Services {
-public class PackageService : IPackageService {
+  public class PackageService : IPackageService {
     private readonly IPackageData _data;
-    public PackageService(IPackageData data) {
-        _data = data;
-    }
+    public PackageService(IPackageData data) { _data = data; }
 
     public async Task<List<Package>> GetPackagesPaginatedAsync(int page,
-            int amount) {
-        return await _data.GetPackagesPaginatedAsync(page, amount);
+                                                               int amount) {
+      return await _data.GetPackagesPaginatedAsync(page, amount);
     }
 
     public async Task<List<Package>> GetTopPackagesAsync(int amount) {
-        return await _data.GetTopPackagesAsync(amount);
+      return await _data.GetTopPackagesAsync(amount);
     }
 
-    public async Task<List<Package>> GetPackagesFromOrganisation(Guid organisationId) {
-        return await _data.GetPackagesFromOrganisation(organisationId);
+    public async Task<List<Package>>
+    GetPackagesFromOrganisation(Guid organisationId) {
+      return await _data.GetPackagesFromOrganisation(organisationId);
     }
 
     public async Task<Package> CreatePackageAsync(Package newPackage) {
-        return await _data.CreatePackageAsync(newPackage);
+      return await _data.CreatePackageAsync(newPackage);
     }
 
     public async Task<Package> GetFullPackageAsync(Guid packageId) {
-        return await _data.GetFullPackageAsync(packageId);
+      return await _data.GetFullPackageAsync(packageId);
     }
 
     public async Task<Package> ArchivePackageAsync(Guid packageId) {
-        return await _data.ArchivePackageAsync(packageId);
+      return await _data.ArchivePackageAsync(packageId);
     }
-}
+  }
 }
--- lpr.WebAPI/Controllers/PackageController.cs
+++ lpr.WebAPI/Controllers/PackageController.cs
@@ -8,93 +8,90 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace lpr.WebAPI.Controllers {
-[ApiController]
-[Route("[controller]")]
-public class PackageController : ControllerBase {
+  [ApiController]
+  [Route("[controller]")]
+  public class PackageController : ControllerBase {
     private readonly IPackageService _srv;
-    public PackageController(IPackageService srv) {
-        _srv = srv;
-    }
+    public PackageController(IPackageService srv) { _srv = srv; }
 
     [HttpGet("GetPackagesPaginated/{page}/{amount}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> GetPackagesPaginated(int page, int amount) {
-        try {
-            List<Package> output = await _srv.GetPackagesPaginatedAsync(page, amount);
-            return StatusCode(200, output);
-        } catch (Exception ex) {
-            return StatusCode(500, new ErrorMessage(ex.Message));
-        }
+    public async Task<IActionResult> GetPackagesPaginated(int page,
+                                                          int amount) {
+      try {
+        List<Package> output =
+            await _srv.GetPackagesPaginatedAsync(page, amount);
+        return StatusCode(200, output);
+      } catch (Exception ex) {
+        return StatusCode(500, new ErrorMessage(ex.Message));
+      }
     }
 
     [HttpGet("GetTopPackages/{amount}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetTopPackages(int amount) {
-        try {
-            List<Package> output = await _srv.GetTopPackagesAsync(amount);
-            return StatusCode(200, output);
-        } catch (Exception ex) {
-            return StatusCode(500, new ErrorMessage(ex.Message));
-        }
+      try {
+        List<Package> output = await _srv.GetTopPackagesAsync(amount);
+        return StatusCode(200, output);
+      } catch (Exception ex) {
+        return StatusCode(500, new ErrorMessage(ex.Message));
+      }
     }
 
     [HttpPost("GetPackagesFromOrganisation/{organisationId}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> GetPackagesFromOrganisation(Guid organisationId)
-    {
-        try
-        {
-            List<Package> output = await _srv.GetPackagesFromOrganisation(organisationId);
-            return StatusCode(200, output);
-        }
-        catch (ArgumentException ex)
-        {
-            return StatusCode(400, new ErrorMessage(ex.Message));
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(500, new ErrorMessage(ex.Message));
-        }
+    public async Task<IActionResult>
+    GetPackagesFromOrganisation(Guid organisationId) {
+      try {
+        List<Package> output =
+            await _srv.GetPackagesFromOrganisation(organisationId);
+        return StatusCode(200, output);
+      } catch (ArgumentException ex) {
+        return StatusCode(400, new ErrorMessage(ex.Message));
+      } catch (Exception ex) {
+        return StatusCode(500, new ErrorMessage(ex.Message));
+      }
     }
 
     [HttpPost("CreatePackage")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> CreatePackage([FromBody] PackageDtoIn newPackage) {
-        try {
-            Package output = await _srv.CreatePackageAsync(new Package(newPackage));
-            return StatusCode(200, output);
-        } catch (Exception ex) {
-            return StatusCode(500, new ErrorMessage(ex.Message));
-        }
+    public async Task<IActionResult>
+    CreatePackage([FromBody] PackageDtoIn newPackage) {
+      try {
+        Package output = await _srv.CreatePackageAsync(new Package(newPackage));
+        return StatusCode(200, output);
+      } catch (Exception ex) {
+        return StatusCode(500, new ErrorMessage(ex.Message));
+      }
     }
 
     [HttpGet("GetFullPackage/{packageId}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> GetFullPackage(Guid packageId) {
-        try {
-            Package output = await _srv.GetFullPackageAsync(packageId);
-            return StatusCode(200, output);
-        } catch (Exception ex) {
-            return StatusCode(500, new ErrorMessage(ex.Message));
-        }
+      try {
+        Package output = await _srv.GetFullPackageAsync(packageId);
+        return StatusCode(200, output);
+      } catch (Exception ex) {
+        return StatusCode(500, new ErrorMessage(ex.Message));
+      }
     }
 
     [HttpDelete("ArchivePackage/{packageId}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> ArchivePackage(Guid packageId) {
-        try {
-            Package output = await _srv.ArchivePackageAsync(packageId);
-            return StatusCode(200, output);
-        } catch (Exception ex) {
-            return StatusCode(500, new ErrorMessage(ex.Message));
-        }
+      try {
+        Package output = await _srv.ArchivePackageAsync(packageId);
+        return StatusCode(200, output);
+      } catch (Exception ex) {
+        return StatusCode(500, new ErrorMessage(ex.Message));
+      }
     }
-}
+  }
 }
