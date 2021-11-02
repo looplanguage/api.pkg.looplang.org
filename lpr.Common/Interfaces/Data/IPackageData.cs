b48fceb348ebf37251c1fbc113c66a830b7714f0
Restyled.io <commits@restyled.io>
2021-11-02 11:10:38 +0000

    Restyled by clang-format

--- lpr.Common/Interfaces/Data/IPackageData.cs
+++ lpr.Common/Interfaces/Data/IPackageData.cs
@@ -1,14 +1,15 @@
 ﻿using lpr.Common.Models;
 
 namespace lpr.Common.Interfaces.Data {
-public interface IPackageData {
+  public interface IPackageData {
     public Task<List<Package>> GetPackagesPaginatedAsync(int page, int amount);
     public Task<List<Package>> GetTopPackagesAsync(int amount);
 
-    public Task<List<Package>> GetPackagesFromOrganisationAsync(Guid organisationId);
+    public Task<List<Package>>
+    GetPackagesFromOrganisationAsync(Guid organisationId);
     public Task<Package>
     CreatePackageAsync(Package newPackage);                   // TODO: Add auth
     public Task<Package> GetFullPackageAsync(Guid packageId); // TODO: Add auth
     public Task<Package> ArchivePackageAsync(Guid packageId); // TODO: Add auth
-}
+  }
 }
