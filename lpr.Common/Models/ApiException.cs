6a78b18261f66f6a80b4425ad6217eb70d538b34
Restyled.io <commits@restyled.io>
2021-11-04 13:25:10 +0000

    Restyled by clang-format

--- lpr.Common/Models/ApiException.cs
+++ lpr.Common/Models/ApiException.cs
@@ -5,18 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace lpr.Common.Models {
-public class ApiException : Exception {
-    public int ErrorCode {
-        get;
-        set;
-    }
-    public ErrorMessage ErrorMessage {
-        get;
-        set;
-    }
+  public class ApiException : Exception {
+    public int ErrorCode { get; set; }
+    public ErrorMessage ErrorMessage { get; set; }
     public ApiException(int errorCode, ErrorMessage errorMessage) {
-        ErrorCode = errorCode;
-        ErrorMessage = errorMessage;
+      ErrorCode = errorCode;
+      ErrorMessage = errorMessage;
     }
-}
+  }
 }
--- lpr.WebAPI/Middleware/GlobalErrorHandler.cs
+++ lpr.WebAPI/Middleware/GlobalErrorHandler.cs
@@ -1,35 +1,27 @@
 ﻿using lpr.Common.Models;
 using System.Text.Json;
 
-namespace lpr.WebAPI.Middleware
-{
-public class GlobalErrorHandler
-{
+namespace lpr.WebAPI.Middleware {
+  public class GlobalErrorHandler {
     private readonly RequestDelegate _next;
 
-    public GlobalErrorHandler(RequestDelegate next)
-    {
-        _next = next;
-    }
+    public GlobalErrorHandler(RequestDelegate next) { _next = next; }
 
-    public async Task Invoke(HttpContext context)
-    {
-        try
-        {
-            await _next(context);
-        } catch (ApiException ex)
-        {
-            HttpResponse response = context.Response;
-            response.ContentType = "application/json";
+    public async Task Invoke(HttpContext context) {
+      try {
+        await _next(context);
+      } catch (ApiException ex) {
+        HttpResponse response = context.Response;
+        response.ContentType = "application/json";
 
-            response.StatusCode = ex.ErrorCode;
-            if(ex.ErrorCode == 401 || ex.ErrorCode == 402 || ex.ErrorCode == 404 || ex.ErrorCode == 500)
-            {
-                await response.WriteAsync(JsonSerializer.Serialize(ex.ErrorMessage));
-                return;
-            }
-            await response.WriteAsync("");
+        response.StatusCode = ex.ErrorCode;
+        if (ex.ErrorCode == 401 || ex.ErrorCode == 402 || ex.ErrorCode == 404 ||
+            ex.ErrorCode == 500) {
+          await response.WriteAsync(JsonSerializer.Serialize(ex.ErrorMessage));
+          return;
         }
+        await response.WriteAsync("");
+      }
     }
-}
+  }
 }
