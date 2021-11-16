49a697c58bd2081be2988b29dbc85e09ced011ac
Kusters,Damien D.G.P <damien.kusters@student.fontys.nl>
2021-11-16 11:43:34 +0100

    Demo authentication with GitHub

--- lpr.Logic/Services/AuthService.cs
+++ lpr.Logic/Services/AuthService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,19 +13,32 @@ namespace lpr.Logic.Services
         //private readonly IAuthData _data;
         //public AuthService(IAuthData data) { _data = data; }
 
-        public async Task<string> ValidateGitHubAccessToken(string token)
+        public const string clientId = "9f93c7a725e27ee50ed3";
+        public const string clientSecret = "<<<SECRET>>>";
+
+        public async Task<string> ValidateGitHubAccessToken(string authKey)
         {
+            var bodyRaw = new {
+                client_id = clientId,
+                client_secret = clientSecret,
+                code = authKey
+            };
+
+            var body = JsonContent.Create(bodyRaw);
+
             //https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://github.com/login/oauth/access_token");
+            client.BaseAddress = new Uri("https://github.com");
 
             client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("LPR", "1.0"));
             client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", token);
 
-            var response = await client.PostAsync("/", null);
-            return response.ToString();
-            //return await _data.ValidateGitHubAccessToken(page, amount);
+            var response = await client.PostAsJsonAsync("/login/oauth/access_token", body.Value);
+            string githubAuth = await response.Content.ReadAsStringAsync();
+            if (githubAuth.Contains("access_token"))
+                return githubAuth;
+            else
+                throw new Exception('Not a valid authKey has been given, is used or expired.');
         }
     }
 }
